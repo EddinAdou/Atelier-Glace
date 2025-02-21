c352eb110e6dc878f65a1367894e8a4b6840ede2
EddinAdou <k.adou@it-students.fr>
2025-02-21 14:52:14 +0100

    add build

--- project/src/components/Toast.tsx
+++ project/src/components/Toast.tsx
@@ -22,7 +22,7 @@ const Toast: React.FC<ToastProps> = ({ message, type, onClose }) => {
     return (
         <div
             className={clsx(
-                'fixed top-16 right-4 z-50 text-white px-4 py-2 rounded shadow-lg',
+                'fixed top-20 right-4 z-50 text-white px-4 py-2 rounded shadow-lg',
                 bgColor
             )}
         >
